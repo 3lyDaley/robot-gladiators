b52253b19e43267b10a6b0ddc5fd7d4bef7e6daa
Bad Boys Club <3ly.daley@gmail.com>
2022-03-27 02:11:40 -0700

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
@@ -12,6 +13,12 @@ var fight = function() {
     // Alert the players they are starting the round.
     window.alert("Welcome to Robot Gladiators!");
 
+    // ask if the player would like to SKIP or FIGHT this round.
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+    console.log(playerName + " has chosen to " + promptFight + ".");
+
+if (promptFight === "FIGHT" || promptFight === "fight"){
     /* Subtract value of playerAttack from enemyHealth
      Update value of enemyHealth var
     */
@@ -46,6 +53,23 @@ var fight = function() {
     else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
+} else if (promptFight === "SKIP" || promptFight === "skip") {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if true, leave fight
+    if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        // subtract money from player for skipping
+        playerMoney = playerMoney - 2;
+    }
+    // if no, ask question again by running fight() again
+    else {
+        fight();
+    }
+} else {
+    window.alert("You need to choose a valid option. Try again!");
+}
 };
 
 fight();
